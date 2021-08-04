1ae2b11fa4be615a393b6aa871dc474fc3dc9929
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2021-08-04 13:23:28 +0200

    add stuff to home

--- apps/lunchbox/src/app/home/home.module.ts
+++ apps/lunchbox/src/app/home/home.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
   ],
   exports: [
     HomeComponent
